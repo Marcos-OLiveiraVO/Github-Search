19c0c4ae237c4a97fd212b19b7f00c197954fc77
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-24 16:58:31 -0300

    Refactor: List-Users Nextpage

--- src/modules/users/List-Users-Github-controller.ts
+++ src/modules/users/List-Users-Github-controller.ts
@@ -4,11 +4,12 @@ import { Request, Response } from "express";
 class ListUsersGithubController {
   async handle(req: Request, res: Response): Promise<Response> {
     try {
-      const number = req.query.id;
-      const { data } = await axios.get(
-        `https://api.github.com/users?since=${number}`
+      const since = req.query.since;
+      const { data, headers } = await axios.get(
+        `https://api.github.com/users?since=${since}`
       );
-      return res.json({ users: data });
+      const linkNextPage = headers.link;
+      return res.json({ users: data, linkNextPage });
     } catch (err) {
       return res.status(404).send({ err: err.message });
     }
