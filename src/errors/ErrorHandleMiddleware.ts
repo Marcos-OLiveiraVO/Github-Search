972f0ea6a9f9db876916150549db440516e7c4fd
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-24 17:21:47 -0300

    refactor: middleware errorApp handle

---
+++ src/errors/ErrorHandleMiddleware.ts
@@ -0,0 +1,19 @@
+import { AppError } from "./AppError";
+import { Request, Response, NextFunction } from "express";
+
+class ErrorHandlerMiddleware {
+  handle(err: Error, req: Request, res: Response, next: NextFunction) {
+    if (err instanceof AppError) {
+      return res.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return res.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+}
+
+export { ErrorHandlerMiddleware };
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import { router } from "./routes";
+import { ErrorHandlerMiddleware } from "./errors/ErrorHandleMiddleware";
 
 const app = express();
 const PORT = 3000;
 
+const errorMiddleware = new ErrorHandlerMiddleware();
+
 app.use(express.json());
 app.use(router);
+
+app.use(errorMiddleware.handle);
 
 app.listen(PORT, () => console.log("Server is running..."));
