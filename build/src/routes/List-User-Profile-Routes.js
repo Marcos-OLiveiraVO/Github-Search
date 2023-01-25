09623ee98510a9d12e04658740e1c612f2cab66e
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-25 11:34:28 -0300

    refactor: build

--- build/src/routes/List-User-Profile-Routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userProfileRoutes = void 0;
-const express_1 = require("express");
-const List_User_Profile_Controller_1 = require("../modules/users/List-User-Profile-Controller");
-const userProfileRoutes = (0, express_1.Router)();
-exports.userProfileRoutes = userProfileRoutes;
-const userProfileDetailsController = new List_User_Profile_Controller_1.ListUserProfileController();
-userProfileRoutes.get("/:username/details", userProfileDetailsController.handle);
--- build/src/routes/list-Users-From-Github-routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.listUsersRouter = void 0;
-const express_1 = require("express");
-const List_Users_From_Github_controller_1 = require("../modules/users/List-Users-From-Github-controller");
-const listUsersRouter = (0, express_1.Router)();
-exports.listUsersRouter = listUsersRouter;
-const listUsersGithubController = new List_Users_From_Github_controller_1.ListUsersFromGithubController();
-listUsersRouter.get("/", listUsersGithubController.handle);
